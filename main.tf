28b22d9237c682f3ba57a4acb474768cc58dca8b
andersnou <anders.nou@gmail.com>
2021-07-15 17:51:28 +0300

    tf-destroy

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = var.region
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-backend-bucket-anou"
+    key    = "test/key"
+    region = "us-west-1"
+  }
+}
+
 resource "aws_s3_bucket" "test_bucket" {
   bucket = "es-test-bucket-12345"
   acl = "private"
