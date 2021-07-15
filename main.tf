ce8a6823e91c73d1e78d8e8a97c2001e89ace47a
andersnou <anders.nou@gmail.com>
2021-07-15 17:20:14 +0300

    Remove test bucket

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
 provider "aws" {
   region = var.region
 }
-
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = format("es-test-bucket-%s", random_pet.prefix.id)
-  acl = "private"
-
-  tags = {
-    Name = "Test bucket"
-  }
-}
-
-resource "random_pet" "prefix" {}
