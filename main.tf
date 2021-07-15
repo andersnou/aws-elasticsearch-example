d3aa2e665195456c678e3b69fa9de550ce94061d
andersnou <anders.nou@gmail.com>
2021-07-15 17:22:12 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = var.region
 }
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = format("es-test-bucket-%s", random_pet.prefix.id)
+  acl = "private"
+
+  tags = {
+    Name = "Test bucket"
+  }
+}
+
+resource "random_pet" "prefix" {}
